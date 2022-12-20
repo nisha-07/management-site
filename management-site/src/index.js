c63dcb03a5e842693ae5c03db428f64f518ad872
nisha-07 <nishasataki@gmail.com>
2022-12-20 15:58:29 +0530

    MS: Create context for authentication

--- management-site/src/index.js
+++ management-site/src/index.js
@@ -2,13 +2,16 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
