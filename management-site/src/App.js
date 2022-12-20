3051c9c71c2d316ac4116c79c331d33d11328fca
nisha-07 <nishasataki@gmail.com>
2022-12-20 14:32:48 +0530

    Initial commit

---
+++ management-site/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4 className='mt-3'>Welcome to my final project!</h4>
+    </div>
+  );
+}
+
+export default App;
