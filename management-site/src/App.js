e12e96c3b1f0cb02121764351ee022d2fc775b16
nisha-07 <nishasataki@gmail.com>
2023-01-03 13:17:35 +0530

    MS: Create a component for onlineuser

--- management-site/src/App.js
+++ management-site/src/App.js
@@ -6,6 +6,7 @@ import Create from "../src/pages/create/Create"
 import Dashboard from "../src/pages/dashboard/Dashboard"
 import Login from "../src/pages/login/Login"
 import Navbar from './components/Navbar/Navbar';
+import OnlineUser from './components/OnlineUser/OnlineUser';
 import Project from "../src/pages/project/Project"
 import Sidebar from './components/Sidebar/Sidebar';
 import Signup from "../src/pages/signup/Signup"
@@ -28,6 +29,7 @@ function App() {
             <Route path="/project/:id" element={user ? <Project /> : <Navigate to="/login" />} />
           </Routes>
         </div>
+        {user && <OnlineUser />}
       </BrowserRouter>
     </div>
   );
---
+++ management-site/src/components/OnlineUser/OnlineUser.js
@@ -0,0 +1,22 @@
+import classes from "./OnlineUser.module.css"
+import { useCollection } from "../../hooks/useCollection"
+
+const OnlineUser = () => {
+    const { documents } = useCollection("users")
+
+    return (
+        <div className={classes.users}>
+            <h5>Users</h5>
+            {documents && documents.map((doc) => {
+                return (
+                    <div key={doc?.id}>
+                        <p>{doc?.displayName}</p>
+                        <img src={doc?.photoURL} alt="avatar" />
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default OnlineUser
