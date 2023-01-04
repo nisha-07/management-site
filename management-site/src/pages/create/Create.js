a8d62c2a30017f4905f9e8f1263b7a5c12882775
nisha-07 <nishasataki@gmail.com>
2023-01-04 11:46:06 +0530

    MS: Create a form for add project

--- management-site/src/pages/create/Create.js
+++ management-site/src/pages/create/Create.js
@@ -1,9 +1,60 @@
 import classes from "./Create.module.css"
+import { useState } from "react"
 
 const Create = () => {
+    const [name, setName] = useState("")
+    const [details, setDetails] = useState("")
+    const [date, setDate] = useState("")
+    const [category, setCategory] = useState("")
+    const [assignedUsers, setAssignedUsers] = useState([])
+
+    // handle event onclick of add project button
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name, details, date, "Project details")
+    }
+
     return (
-        <div>
-            Create
+        <div className={classes.container}>
+            <h5>Create a new project</h5>
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <label>
+                    <span>Project name:</span>
+                    <input
+                        required
+                        type="text"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                </label>
+                <label>
+                    <span>Project details:</span>
+                    <textarea
+                        required
+                        type="textarea"
+                        onChange={(e) => setDetails(e.target.value)}
+                        value={details}
+                    />
+                </label>
+                <label>
+                    <span>Set due date:</span>
+                    <input
+                        required
+                        type="date"
+                        onChange={(e) => setDate(e.target.value)}
+                        value={date}
+                    />
+                </label>
+                <label>
+                    <span>Project category:</span>
+                </label>
+                <label>
+                    <span>Assign to:</span>
+                </label>
+                <button type="submit" className={classes.btn}>
+                    Add Project
+                </button>
+            </form>
         </div>
     )
 }
